3a976ebb5163cb9c95b52c0dfd7844b4f6768fea
gkemmey <gray.kemmey@gmail.com>
2019-03-04 16:22:49 -0500

    make sprinkles simpler

--- app/assets/javascript/sprinkles/todos.js
+++ app/assets/javascript/sprinkles/todos.js
@@ -3,21 +3,13 @@ on(document, "click", "[data-behavior~=submit_form_when_clicked]", function(even
 });
 
 on(document, "focusout", "[data-behavior~=submit_form_when_blurred]", function(event) {
-  this.closest("form").submit();
+  if (this.closest(".editing[data-behavior~=double_click_to_edit]")) {
+    this.closest("form").submit();
+  }
 });
 
 on(document, "dblclick", "[data-behavior~=double_click_to_edit]", function(event) {
   this.classList.add("editing");
-
-  var children_to_hide = this.querySelectorAll("[data-behavior~=hidden_after_double_clicking_to_edit]");
-  for (var i = 0; i < children_to_hide.length; ++i) {
-    children_to_hide[i].style.display = 'none';
-  }
-
-  var children_to_show = this.querySelectorAll("[data-behavior~=visible_after_double_clicking_to_edit]");
-  for (var i = 0; i < children_to_show.length; ++i) {
-    children_to_show[i].style.display = '';
-  }
 
   this.querySelector("input.edit").focus();
   this.querySelector("input.edit").select();
@@ -28,14 +20,4 @@ on(document, "keydown", "[data-behavior~=cancel_edit_on_escape]", function(event
 
   var li = this.closest("[data-behavior~=double_click_to_edit]");
   li.classList.remove("editing");
-
-  var children_to_hide = li.querySelectorAll("[data-behavior~=visible_after_double_clicking_to_edit]");
-  for (var i = 0; i < children_to_hide.length; ++i) {
-    children_to_hide[i].style.display = 'none';
-  }
-
-  var children_to_show = li.querySelectorAll("[data-behavior~=hidden_after_double_clicking_to_edit]");
-  for (var i = 0; i < children_to_show.length; ++i) {
-    children_to_show[i].style.display = '';
-  }
 });
